85f12eebc4bfba404bfef0b4fc9a4e3330873148
unknown <usuario@ecommerce03-pc.DOMAIN.local>
2026-08-27 14:38:19 -0300

    arruma html

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const filmes = [
-  { t: "Ghost", img: "https://via.placeholder.com/300x450?text=Ghost" },
-  { t: "Ghostbusters", img: "https://via.placeholder.com/300x450?text=Ghostbusters" },
-  { t: "Ghost Rider", img: "https://via.placeholder.com/300x450?text=Ghost+Rider" }
+	{ t: "Ghost", img: "https://via.placeholder.com/300x450?text=Ghost" },
+	{ t: "Ghostbusters", img: "https://via.placeholder.com/300x450?text=Ghostbusters" },
+	{ t: "Ghost Rider", img: "https://via.placeholder.com/300x450?text=Ghost+Rider" }
 ];
 
 function mostrar(lista) {
-  const container = document.getElementById("grid");
-  container.innerHTML = "";
-  lista.forEach(filme => {
-    container.innerHTML += `
-      <div class="card">
-        <img src="${filme.img}">
-        <h3>${filme.t}</h3>
-      </div>
-    `;
-  });
+	const container = document.getElementById("grid");
+	container.innerHTML = "";
+	lista.forEach(filme => {
+		container.innerHTML += `
+			<div class="card">
+				<img src="${filme.img}" style="width:100%">
+				<h3>${filme.t}</h3>
+			</div>
+		`;
+	});
 }
 
 mostrar(filmes);
