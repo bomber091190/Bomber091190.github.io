eba87d86bf6eb2ec79aa8fea62a5e8e7db235fc0
unknown <usuario@ecommerce03-pc.DOMAIN.local>
2026-08-27 14:14:23 -0300

    corrige filmes com chaves

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const filmes = [
-	{ t: "Ghost - Do Outro Lado da Vida", img: "https://m.media-amazon.com/images/MV5BNTgyY2Y0ODUtYjQyYi00NjJhLTg4Y2EtMTAzMTEzYjI2MzZkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg" },
+	{ t: "Ghost - Do Outro Lado da Vida", img: "https://m.media-amazon.com/images/M/MV5BNTgyY2Y0ODUtYjQyYi00NjJhLTg4Y2EtMTAzMTEzYjI2MzZkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg" },
 	{ t: "Ghostbusters", img: "https://m.media-amazon.com/images/M/MV5BMTQ0MDE5MTg4M15BMl5BanBnXkFtZTcwMzYzOTQyMw@@._V1_.jpg" },
 	{ t: "Ghost Rider", img: "https://m.media-amazon.com/images/M/MV5BMTk4OTQzMDE1OF5BMl5BanBnXkFtZTcwMDI5MTUyMQ@@._V1_.jpg" }
 ];
